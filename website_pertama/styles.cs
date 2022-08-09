c46dd143abb30043dd518958ef6a8508e1379161
Fajar416 <110881311+Fajar416@users.noreply.github.com>
2022-08-09 12:11:42 +0700

    Add files via upload

---
+++ website_pertama/styles.cs
@@ -0,0 +1,39 @@
+*{
+  box-sizign: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'kumbh sans', sans-serif;
+  scroll-behavior
+}
+
+.navbar {
+  background:    #131313
+   height: 80x;
+   display: flex;
+   justify-content: center;
+   align-item: center;
+   font-size: 1.2rem;
+   position: stiky;
+   top: 0;
+   z-index: 999;
+}
+.navbar__container {
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  widht: 100%;
+  max-widht: 1300px;
+  margin: 0 auto;
+  padding: 0 50px
+}
+
+#navbar__logo {
+  background-color: #ff
+  background-image: linear-gradiant(to top, #ff0844 0%, #ffb199 100%)
+  background-size: 100%
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;n
+}
